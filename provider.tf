faffbb290f57c76fa557f68ba746fe526ff2ccf2
Vishnu <115578864+Vishnu068709@users.noreply.github.com>
2024-11-04 09:47:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("credentials.json")}"
-project = "ac-shared-playground"
-region = "us-central1"
+  credentials = file("C:/Users/User/Downloads/gcpterraform-432705-9fe28c595a45.json")
+  project     = "gcpterraform-432705"
+  region      = "us-central1"
 }
